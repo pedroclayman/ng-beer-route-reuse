c53bd388729a2b9c6679515c7e91d6ee0bcf53c5
Peter Mihalik <peter.mihalik@exponea.com>
2022-08-19 14:18:26 +0200

    Step 6: fix updating the employee list

--- src/app/employees/employee-list/employee-list-page.component.ts
+++ src/app/employees/employee-list/employee-list-page.component.ts
@@ -1,19 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Employee} from "../employees.types";
 import {ActivatedRoute} from "@angular/router";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-employee-list-page',
   templateUrl: './employee-list-page.component.html',
   styleUrls: ['./employee-list-page.component.scss']
 })
-export class EmployeeListPageComponent implements OnInit {
-  readonly employees: Employee[];
+export class EmployeeListPageComponent implements OnInit, OnDestroy {
+  employees: Employee[] | undefined;
+  private readonly destroy$ = new Subject<void>();
 
   constructor(activatedRoute: ActivatedRoute) {
-    this.employees = activatedRoute.snapshot.data.employees as Employee[];
+    activatedRoute.data.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(data => {
+
+      this.employees = data.employees as Employee[];
+    })
   }
+
   ngOnInit(): void {
   }
 
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
