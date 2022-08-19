b6733d0b0488b158a46da056b61410424ceccd1e
Peter Mihalik <peter.mihalik@exponea.com>
2022-08-19 14:30:35 +0200

    Step 9: fix updating the employee detail

--- src/app/employees/employee-detail-page/employee-detail-page.component.ts
+++ src/app/employees/employee-detail-page/employee-detail-page.component.ts
@@ -1,20 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Employee} from "../employees.types";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-employee-detail-page',
   templateUrl: './employee-detail-page.component.html',
   styleUrls: ['./employee-detail-page.component.scss']
 })
-export class EmployeeDetailPageComponent implements OnInit {
-  readonly employee: Employee | undefined;
+export class EmployeeDetailPageComponent implements OnDestroy {
+  employee: Employee | undefined;
+  private readonly destroy$ = new Subject<void>();
 
   constructor(activatedRoute: ActivatedRoute) {
-    this.employee = activatedRoute.snapshot.data.employee as Employee;
+    activatedRoute.data.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(data => this.employee = data.employee as Employee);
   }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
